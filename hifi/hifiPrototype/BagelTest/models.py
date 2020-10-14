9e9990c6e4f3ce02baad2b78237a325e6ec90305
JPope <jacob.pope@aggiemail.usu.edu>
2020-10-13 19:18:22 -0600

    set up a basic templates and database

--- hifi/hifiPrototype/BagelTest/models.py
+++ hifi/hifiPrototype/BagelTest/models.py
@@ -1,3 +1,17 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Order(models.Model):
+    customer_name = models.CharField(max_length=50)
+    pickup_time = models.DateTimeField('pickup_time')
+
+
+    def __str__(self):
+        return self.customer_name
+
+
+
+class Menu_Item(models.Model):
+    item_name = models.ForeignKey(Order, on_delete=models.CASCADE)
--- hifi/hifiPrototype/BagelTest/views.py
+++ hifi/hifiPrototype/BagelTest/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Order
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World")
+def customer(request):
+    return HttpResponse("Customer Page")
     # return render(request, 'BagelTest/index.html', context)
+
+def employee(request):
+    order_list = Order.objects.order_by('-pickup_time')
+    context = {'order_list': order_list,}
+    return render(request, 'BagelTest/employee.html', context)
