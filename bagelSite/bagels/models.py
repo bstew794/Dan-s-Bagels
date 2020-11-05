198e8e9b3317ae6c947f308b2562e8313c979519
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-05 14:42:33 -0700

    Added Profile model.

--- bagelSite/bagels/models.py
+++ bagelSite/bagels/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# An Order can only have one Customer but a Customer can have many Orders
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phoneNumber = models.CharField(max_length=50, default='')
+    memberNumber = models.CharField(max_length=50, default='')
+    cardNumber = models.CharField(max_length=50, default='')
+    accountBalance = models.DecimalField(decimal_places=2)
+    
