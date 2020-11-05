4e1fc9d769ba168f462df139b9dba2d34a5c0675
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-05 14:58:53 -0700

    Added InventoryItem model

--- bagelSite/bagels/models.py
+++ bagelSite/bagels/models.py
@@ -2,7 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-# An Order can only have one Customer but a Customer can have many Orders
+# An Order can only have one User but a User can have many Orders
+
+class InventoryItem(models.Model):
+    price = models.DecimalField(decimal_places=2)
+    stock = models.IngeterField()
+    name = models.CharField(max_length=50, default='')
+    description = models.TextField()
+    allegryInfo = models.CharField(max_length=50, default='')
+    
+    ITEMTYPE = (
+        ('ba', 'bagel'),
+        ('be', 'beverage'),
+        ('sc', 'Schmear'),
+        ('sa', 'sandwich'),
+    )
+    
+    type = models.CharField(
+        max_length=2,
+        choices=ITEMTYPE,
+        blank=True,
+        default='ba',
+        help_text='Item type',
+    )
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phoneNumber = models.CharField(max_length=50, default='')
