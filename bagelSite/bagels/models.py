266bc26b94cb625d595dabb282c5524dff44f284
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-05 16:02:16 -0700

    Added Order model

--- bagelSite/bagels/models.py
+++ bagelSite/bagels/models.py
@@ -1,8 +1,27 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
 # An Order can only have one User but a User can have many Orders
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phoneNumber = models.CharField(max_length=50, default='')
+    memberNumber = models.CharField(max_length=50, default='')
+    cardNumber = models.CharField(max_length=50, default='')
+    accountBalance = models.DecimalField(decimal_places=2)
+
+class Order(models.Model):
+    customer = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='orders')
+    customerName = models.CharField(max_length=50, default='')
+    pickUpTime = models.DateTimeField('pickUpTime')
+    items = models.CharField(default='[]')
+    totalCost = models.DecimalField(decimal_places=2)
+    isPrepared = models.BooleanField()
+    isFufilled = models.BooleanField()
+    
 
 class InventoryItem(models.Model):
     price = models.DecimalField(decimal_places=2)
@@ -24,12 +43,4 @@ class InventoryItem(models.Model):
         blank=True,
         default='ba',
         help_text='Item type',
-    )
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phoneNumber = models.CharField(max_length=50, default='')
-    memberNumber = models.CharField(max_length=50, default='')
-    cardNumber = models.CharField(max_length=50, default='')
-    accountBalance = models.DecimalField(decimal_places=2)
-    
+    )    
