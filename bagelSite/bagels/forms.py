f3f5039c2f3c525dcf5996df6f5d82c905d91b06
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-10 10:30:04 -0700

    Added SignUpForm()

--- bagelSite/bagels/forms.py
+++ bagelSite/bagels/forms.py
@@ -1,1 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, help_text='Required.')
+    last_name = forms.CharField(max_length=30, help_text='Required.')
+    email = forms.EmailField(max_length=254, help_text='Required. Must be a valid email address.')
+    phone_number = forms.CharField(max_length=15, 'Required. Format: XXXXXXXXXXXXXXX')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'phone_number', 'email', 'password1', 'password2', )
