69cd8ae7e0909f6d6057ac85ac189c2900a18fef
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-10 11:03:11 -0700

    Added signUp View

--- bagelSite/bagels/views.py
+++ bagelSite/bagels/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from bagelSite.bagels.forms import SignUpForm
 
 # Create your views here.
+
+
+def signUp(request):
+    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.phone_number = form.cleaned_data.get('phone_number')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            
+            return redirect('BLANK')
+        
+    else:
+        form = SignUpForm()
+        
+    return render(request, 'signup.html', {'form': form})
