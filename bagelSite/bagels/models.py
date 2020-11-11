2db8874c6cabb25ba350850841479a7fedf76ca6
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-10 17:48:48 -0700

    Forgot to import the right modules

--- bagelSite/bagels/models.py
+++ bagelSite/bagels/models.py
@@ -1,4 +1,6 @@
 import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
