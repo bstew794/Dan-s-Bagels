9d8f7efa6edbcd75bc6a17329ab4cdfe699d0e9b
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-10 18:10:37 -0700

    Update forms.py

--- bagelSite/bagels/forms.py
+++ bagelSite/bagels/forms.py
@@ -6,7 +6,7 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, help_text='Required.')
     last_name = forms.CharField(max_length=30, help_text='Required.')
     email = forms.EmailField(max_length=254, help_text='Required. Must be a valid email address.')
-    phone_number = forms.CharField(max_length=15, 'Required. Format: XXXXXXXXXXXXXXX')
+    phone_number = forms.CharField(max_length=15, help_text='Required. Format: XXXXXXXXXXXXXXX')
 
     class Meta:
         model = User
