d45b0b1518b78571cb2bf6986abc96fcb5def8eb
bstew794 <71299552+bstew794@users.noreply.github.com>
2020-11-10 17:13:08 -0700

    added BagelsConfig

--- bagelSite/bagelSite/settings.py
+++ bagelSite/bagelSite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bagels.apps.BagelsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
