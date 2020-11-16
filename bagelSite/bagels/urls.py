cb1f44853f85ace0a439fd0a6401df0248e9a1aa
JPope <jacob.pope@aggiemail.usu.edu>
2020-11-16 16:17:50 -0700

    changed login page to be default for site

--- bagelSite/bagels/urls.py
+++ bagelSite/bagels/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name='bagels/login.html'), name='login'),   
     path('signup', views.signUp, name='signup'),
     path('login', auth_views.LoginView.as_view(template_name='bagels/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
