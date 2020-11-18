03e00028bf4057eba9bce5f1a0081d350eb9b0f1
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2020-11-18 12:00:46 -0700

    register Profile model with admin site

--- bagelSite/bagels/admin.py
+++ bagelSite/bagels/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import Permission
-from .models import Order, InventoryItem
+from .models import Order, InventoryItem, Profile
 
 # Register your models here.
 admin.site.register(Permission)
 admin.site.register(Order)
 admin.site.register(InventoryItem)
+admin.site.register(Profile)
