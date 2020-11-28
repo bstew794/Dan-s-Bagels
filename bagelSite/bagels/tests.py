eb0ff5c23d6c8da4492bd209065d6a6f94c38179
Jonas Williams-Gilchrist <jonas.williams.gilchrist@aggiemail.usu.edu>
2020-11-28 16:11:09 -0700

    added test for incrementing invetory

--- bagelSite/bagels/tests.py
+++ bagelSite/bagels/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, InventoryItem
 
 # Create your tests here.
 
@@ -15,6 +15,11 @@ class ProfileTestCase(TestCase):
         user.profile.member_number = user.profile.phone_number + str(int(round(user.date_joined.timestamp() * 1000)))
         user.save()
 
+        # test_increment_inventory
+        test_item = InventoryItem.objects.create(price=2.99, stock=10, name="test item",
+                                                 description="hello, this is a test", allegry_info="none")
+        test_item.save()
+
     def test_users_can_login(self):
         tester = User.objects.get(username='tester')
         client = Client()
@@ -26,3 +31,9 @@ class ProfileTestCase(TestCase):
         self.assertEqual(tester.profile.phone_number, '9999999999', "Phone number is incorrect.")
         self.assertEqual(tester.profile.member_number, tester.profile.phone_number +
                          str(int(round(tester.date_joined.timestamp() * 1000))), "Member number is incorrect")
+
+    def test_increment_inventory(self):
+        test_item = InventoryItem.objects.get(name="test item")
+        self.assertEqual(test_item.stock, 10)
+        test_item.stock += 1
+        self.assertEqual(test_item.stock, 11)
